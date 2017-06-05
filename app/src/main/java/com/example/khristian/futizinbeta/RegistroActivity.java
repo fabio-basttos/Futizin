6deec554dec174d8261186e45d2566b2ba91294d
Fabio <fabio_basttos@outlook.com>
2017-06-05 11:45:33 -0300

    RegistroRequest parametros

--- app/src/main/java/com/example/khristian/futizinbeta/RegistroActivity.java
+++ app/src/main/java/com/example/khristian/futizinbeta/RegistroActivity.java
@@ -1,40 +1,89 @@
 package com.example.khristian.futizinbeta;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by Khristian on 11/05/2017.
  */
 
 public class RegistroActivity extends Activity {
-    EditText etNomeUsuario;
-    EditText etEmailUsuario;
-    EditText etDataNascimentoUsuario;
-    EditText etContatoUsuario;
-    EditText etSenhaUsuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar_usuario);
 
-        etNomeUsuario = (EditText) findViewById(R.id.etNomeUsuario);
-        etEmailUsuario = (EditText) findViewById(R.id.etEmailUsuario);
-        etDataNascimentoUsuario = (EditText) findViewById(R.id.etDataNascimentoUsuario);
-        etContatoUsuario = (EditText) findViewById(R.id.etContatoUsuario);
-        etSenhaUsuario = (EditText) findViewById(R.id.etSenhaUsuario);
+        final EditText etNomeUsuario = (EditText) findViewById(R.id.etNomeUsuario);
+        final EditText etEmailUsuario = (EditText) findViewById(R.id.etEmailUsuario);
+        final EditText etDataNascimentoUsuario = (EditText) findViewById(R.id.etDataNascimentoUsuario);
+        final EditText etContatoUsuario = (EditText) findViewById(R.id.etContatoUsuario);
+        final EditText etLoginUsuario = (EditText) findViewById(R.id.etLoginUsuario);
+        final EditText etSenhaUsuario = (EditText) findViewById(R.id.etSenhaUsuario);
+        final Button btn_RegistrarUsuario = (Button) findViewById(R.id.btn_RegistrarUsuario);
+
+        btn_RegistrarUsuario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String nome_usuario = etNomeUsuario.getText().toString();
+                final String email_usuario = etEmailUsuario.getText().toString();
+                final String data_nasc_usuario = etDataNascimentoUsuario.getText().toString();
+                final String contato_usuario = etContatoUsuario.getText().toString();
+                final String login_usuario = etLoginUsuario.getText().toString();
+                final String senha_usuario = etSenhaUsuario.getText().toString();
+
+
+                Response.Listener<String> responseListener = new Response.Listener<String>(){
+
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean sucesso = jsonResponse.getBoolean("sucesso");
+
+                            if (sucesso){
+                                Intent intent = new Intent(RegistroActivity.this, LoginActivity.class);
+                                RegistroActivity.this.startActivity(intent);
+                            }
+                            else {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(RegistroActivity.this);
+                                builder.setMessage("Não foi possivel gravar registros")
+                                        .setNegativeButton("Tente novamente", null)
+                                        .create()
+                                        .show();
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+
+                RegistroRequest registroRequest = new RegistroRequest(nome_usuario, email_usuario, data_nasc_usuario, contato_usuario, login_usuario, senha_usuario, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(RegistroActivity.this);
+                queue.add(registroRequest);
+            }
+        });
 
     }
 
-    public void register(View view) {
+    /*public void register(View view) {
         boolean granted = true;
 
-        if (etNomeUsuario.getText().toString().isEmpty()) {
+        if (nome_usuario.getText().toString().isEmpty()) {
             etNomeUsuario.setBackground(getDrawable(R.drawable.border_error));
             Toast.makeText(this, "Campo Incorreto!.", Toast.LENGTH_SHORT).show();
             granted = false;
@@ -74,7 +123,7 @@ public class RegistroActivity extends Activity {
             Toast.makeText(this, "Registrado Com Sucesso!.", Toast.LENGTH_SHORT).show();
             Intent intent = new Intent(RegistroActivity.this, LoginActivity.class);
             startActivity(intent);
-        }
-    }
+        }*/
+
 }
 
--- app/src/main/java/com/example/khristian/futizinbeta/RegistroRequest.java
+++ app/src/main/java/com/example/khristian/futizinbeta/RegistroRequest.java
@@ -1,8 +1,35 @@
 package com.example.khristian.futizinbeta;
 
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 /**
  * Created by Khristian on 11/05/2017.
  */
 
-public class RegistroRequest {
+public class RegistroRequest extends StringRequest {
+    private static final String REQUEST_USUARIO_INSERT_URL = "http://doevida.pe.hu/insertUsuarioFut.php?";
+    private Map<String, String> params;
+
+    public RegistroRequest (String nome_usuario, String email_usuario, String data_nasc_usuario, String contato_usuario, String login_usuario, String senha_usuario, Response.Listener<String> listener) {
+        super(Method.POST, REQUEST_USUARIO_INSERT_URL, listener, null);
+        params = new HashMap<>();
+        params.put("nome_usuario", nome_usuario);
+        params.put("email_usuario", email_usuario);
+        params.put("data_nasc_usuario", data_nasc_usuario);
+        params.put("contato_usuario", contato_usuario);
+        params.put("login_usuario", login_usuario);
+        params.put("senha_usuario", senha_usuario);
+    }
+
+    @Override
+
+    public Map<String, String> getParams() {
+        return params;
+    }
+
 }
