53e9a77d40c15fb205b527d410fd797758f041d7
fabio <fabio_basttos@outlook.com>
2017-06-07 01:22:17 -0300

    inclusão quantidadeJogadores na classe Partida, partida_item.xml, inclusão ListaPartidasActivity

--- app/src/main/java/com/example/khristian/futizinbeta/Partida.java
+++ app/src/main/java/com/example/khristian/futizinbeta/Partida.java
@@ -9,18 +9,20 @@ public class Partida {
     private String local;
     private String data;
     private String horario;
-    private String faixaEtaria;
+    private int faixaEtaria;
     private int valor;
+    private int quantidadeJogadores;
 
     public Partida(){}//INICIANDO O CONSTRUTOR QUE TEM A FUNCAO DE INICIAR O OBJETO.
 
     //ESTA FUNCAO FAZ A REFERENCIA AO PROPRIO OBJETO.
-    public Partida(Long ID, String local, String data, String horario, String faixaEtaria, int valor){
+    public Partida(Long ID, String local, String data, String horario, int faixaEtaria, int quantidadeJogadores, int valor){
         this.ID = ID;
         this.local = local;
         this.data = data;
         this.horario = horario;
         this.faixaEtaria = faixaEtaria;
+        this.quantidadeJogadores = quantidadeJogadores;
         this.valor = valor;
     }
     //ESTA FUNCAO FAZ A REFERENCIA AO PROPRIO OBJETO.
@@ -42,10 +44,13 @@ public class Partida {
     public String getHorario() {return horario;}
     public void setHorario(String horario) {this.horario = horario;}
 
-    public String getFaixaEtaria() {return faixaEtaria;}
-    public void setFaixaEtaria(String faixaEtaria ) {this.faixaEtaria = faixaEtaria;}
+    public int getFaixaEtaria() {return faixaEtaria;}
+    public void setFaixaEtaria(int faixaEtaria ) {this.faixaEtaria = faixaEtaria;}
 
     public int getValor() {return valor;}
     public void setValor(int valor) {this.valor = valor;}
-    // OS DADOS GET E SET POSSIBILITAM A MANIPULACAO DOS DADOS INSERIDOS NAS VARIAVEIS.
+
+    public int getQuantidadeJogadores() {return quantidadeJogadores;}
+    public void setQuantidadeJogadores(int quantidadeJogadores) {this.quantidadeJogadores = quantidadeJogadores;}
+// OS DADOS GET E SET POSSIBILITAM A MANIPULACAO DOS DADOS INSERIDOS NAS VARIAVEIS.
 }
